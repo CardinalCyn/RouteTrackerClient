7367319646bf35ddf581b2397458457da2c24c78
Salah Zanabili <Zanabilis@gmail.com>
2023-05-13 05:36:01 -0500

    production url update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production:true,
     mapsApiKey:'AIzaSyCmubTnRrlJe3ubQsiS_dhjGusxdf5JDU4',
-    apiUrl:"https://www.idk.com"
+    apiUrl:"https://api.route-saver.com"
 };
