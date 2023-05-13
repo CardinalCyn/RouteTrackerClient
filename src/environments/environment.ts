ef75d21942636e9ce21b213b30ebc813900f2347
Salah Zanabili <Zanabilis@gmail.com>
2023-05-13 05:38:43 -0500

    forgot to add / at end of prod url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production:true,
     mapsApiKey:'AIzaSyCmubTnRrlJe3ubQsiS_dhjGusxdf5JDU4',
-    apiUrl:"https://api.route-saver.com"
+    apiUrl:"https://api.route-saver.com/"
 };
